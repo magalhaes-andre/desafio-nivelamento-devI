a03059dc97e7cf1a38d397cd6a465ec5c825ddc5
Magalhães <arfddem@gmail.com>
2019-08-18 15:32:41 -0300

    Listing of guests already working

--- src/main/resources/public/scripts/main.js
+++ src/main/resources/public/scripts/main.js
@@ -1,12 +1,25 @@
 const api = "http://localhost:8080/guestbook";
+let guests = [];
+
+function readFormData(){
+    var data = {};
+    data["event"] = document.getElementById("event").value;
+    data["guests"] = guests;
+    data["place"] = document.getElementById("place").value;
+    console.log(data);
+    return data;
+}
+
+function showGuests(){
+    let tableBuilder = "";
+    guests.forEach(guest => {
+        tableBuilder += '<li>'+guest.name+'</li>'
+    })
+    document.getElementById("guestList").innerHTML = tableBuilder;
+}
 
 function postEvent(){
-    var formData = {};
-    formData["event"] = document.getElementById("event").value;
-    formData["guests"] = document.getElementById("guest").value;
-    formData["place"] = document.getElementById("place").value;
-
-    alert(JSON.stringify(formData))
+    var formData = readFormData();
     return fetch( api,{
         method: "POST",
         headers: {
@@ -14,5 +27,28 @@ function postEvent(){
         },
         body: JSON.stringify(formData)
     })
-    .then(response => response.JSON());
+    .then(response => response.json());
+}
+
+function addGuest(){
+    if(guestIsPresent()){
+        alert("Already have this guest on the list ;)")
+    }else{
+        guests.push({name:getGuest()})
+    }
+    showGuests()
+}
+
+function guestIsPresent(){
+    isPresent = false;
+    guests.forEach(guest => {
+        if (guest.name == getGuest()){
+            isPresent = true;
+        }
+    })
+    return isPresent;
+}
+
+function getGuest(){
+    return document.getElementById("guest").value
 }
