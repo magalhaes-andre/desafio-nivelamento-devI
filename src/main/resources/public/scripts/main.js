6f3097fed41df4a5b846f19c6cd5fc5842d00425
Magalhães <arfddem@gmail.com>
2019-08-17 23:38:46 -0300

    Refactoring the front end completely.

--- src/main/resources/public/scripts/main.js
+++ src/main/resources/public/scripts/main.js
@@ -1,37 +1,18 @@
-var lista = [];
-const api = "http://localhost:8080";
-   
-function preencheLista () {
-    lista.push({nome: document.getElementById('convidado').value});
-    console.log(lista);
-    var linha = document.createElement("LI");
-    var conteudo = document.createTextNode(document.getElementById('convidado').value);
-    linha.appendChild(conteudo);
-    document.getElementById("lista-convidados").appendChild(linha);
-}
+const api = "http://localhost:8080/guestbook";
 
-function gravaEvento() {
-    var evento = { nome:document.getElementById("evento").value,
-        convidados: lista,
-        local: document.getElementById("local").value
-    };
-    var request = new XMLHttpRequest();
-    var eventoMontado = JSON.stringify(evento);
-    request.open("POST", api+"/adicionaEvento", false);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.onload = function() {
-          listaEventos();
-    };
-    request.send(eventoMontado)
-}
+function postEvent(){
+    var formData = {};
+    formData["event"] = document.getElementById("event").value;
+    formData["guests"] = document.getElementById("guest").value;
+    formData["place"] = document.getElementById("place").value;
 
-function listaEventos() {
-        var request = new XMLHttpRequest();
-        request.open("GET", api+'/listaEventos', true);
-        request.send();
-        console.log(this.response)
+    alert(JSON.stringify(formData))
+    return fetch( api,{
+        method: "POST",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.JSON());
 }
-      
-listaEventos();
-
-      
